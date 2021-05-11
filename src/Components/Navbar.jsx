662bf20da0bbe32a8addf6a5171bda89617ec712
tatendabakozw <tatendabakozw@gmail.com>
2021-05-11 21:41:19 +0200

    our jobs

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar(props) {
                       : "text-gray-800 hover:text-gray-600") +
                     " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
                   }>
-                  Services
+                  Our Jobs
                 </Link>
               </li>
 
